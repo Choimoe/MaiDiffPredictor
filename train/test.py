f6a7e5c3bebe94832808ab981ce0507fef9599a5
Choimoe <qwq.shq@gmail.com>
2025-05-01 16:19:12 +0800

    fix(test): using the dataset chart directly for validation, reinforcing is unnecessary.

--- train/test.py
+++ train/test.py
@@ -8,12 +8,12 @@ sys.path.append(project_root)
 from tools.inference.src.enhance_json_with_features import process_json_file
 from tools.inference.src.inference import predict_difficulty
 
-json_file_path = "serialized_data/11663_5.json"
+json_file_path = "serialized_data/11663_2.json"
 
 def main():
     process_json_file(json_file_path, "chart.json")
 
-    file_name, predicted_difficulty = predict_difficulty("chart.json")
+    file_name, predicted_difficulty = predict_difficulty(json_file_path)
     print(f"文件名: {file_name}")
     print(f"预测的难度: {predicted_difficulty}")
 
